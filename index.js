d842c527b665cf8416da74400bd5c6b95f30d184
Prathamgarg17 <prathamgargji2002@gmail.com>
2024-02-23 16:19:36 +0530

    changes

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ app.use(bodyParser.json());
 app.post("/translate", (req, res) => {
   const { text, to } = req.body;
   translatte(text, { to })
-    .then((data) => res.send({ text: data.text }))
-    .catch((err) => res.send({ err: err }));
+    .then((data) => res.json({ translation: data.text }))
+    .catch((err) => res.json({ message: err.message, status: err.status }));
 });
 
 app.listen(3000, () => console.log("Server is running on port 3000"));
