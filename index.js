bf1d93219646e4c0ec07dfc892bac1b9c0135b57
Prathamgarg17 <prathamgargji2002@gmail.com>
2024-02-23 16:19:48 +0530

    change

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ app.use(bodyParser.json());
 app.post("/translate", (req, res) => {
   const { text, to } = req.body;
   translatte(text, { to })
+  
     .then((data) => res.json({ translation: data.text }))
     .catch((err) => res.json({ message: err.message, status: err.status }));
 });
